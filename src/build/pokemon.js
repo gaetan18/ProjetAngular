3e1800ceb0871c0f76c3597074a182ca09fa5431
gaetan <demosmonster@gmail.com>
2019-05-20 12:49:21 +0200

    Mise en place Timescript

---
+++ src/build/pokemon.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var pokemon = /** @class */ (function () {
+    function pokemon(name) {
+        this._name = name;
+    }
+    return pokemon;
+}());
+exports.pokemon = pokemon;
---
+++ src/pokemon.ts
@@ -0,0 +1,8 @@
+export class pokemon {
+    _name : string;
+
+    constructor(name : string){
+        this._name = name;
+    }
+
+}
